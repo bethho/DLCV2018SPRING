60d2f217ffb54d202a4e7e248b58fc6144069703
Beth Ho <beth@email>
2018-05-01 19:13:32 +0800

    add the address of model

--- hw3/hw3.sh
+++ hw3/hw3.sh
@@ -1,1 +1,3 @@
+wget 'https://www.dropbox.com/s/sduk4xosp3nvh7t/FCNN32_model_best.h5'
+wget 'https://www.dropbox.com/s/mksamm1em4y6e60/vgg16_weights_tf_dim_ordering_tf_kernels.h5'
 python3 hw3_FCNN32_test.py $1 $2
--- hw3/hw3_best.sh
+++ hw3/hw3_best.sh
@@ -1,1 +1,3 @@
+wget 'https://www.dropbox.com/s/twkb7ohm02ydxq4/U_net_VGG16_model_best.h5'
+wget 'https://www.dropbox.com/s/mksamm1em4y6e60/vgg16_weights_tf_dim_ordering_tf_kernels.h5'
 python3 hw3_U_net_test.py $1 $2
